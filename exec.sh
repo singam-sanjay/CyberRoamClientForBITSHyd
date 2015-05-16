920db5b132316101e0a66d517b9587f7c6f40afb
Sanjay <sssv96@gmail.com>
2015-05-17 04:02:10 +0530

    Added the file which executes to login in to the server(exec.sh). Also added a few cosmetic touches to the install.sh file.

---
+++ exec.sh
@@ -0,0 +1,26 @@
+if [ -e "/tmp/CyberClient.*" ] ; then 
+	rm "/tmp/CyberClient.*"
+fi
+
+if [ ! -e CR_ID.txt ] ; then
+	echo -n "ID (f20**###):"; read CR_ID;
+	echo -n "$CR_ID" >  CR_ID.txt
+	unset CR_ID
+fi
+
+
+if [ ! -e ~/CyberClient.conf ] ; then
+	CR_IP="172.16.0.30" #default is of BITS Hyderabad
+	echo -n "Password :"; read CR_PSSWD;
+	CR_ID='cat CR_ID.txt'
+	echo -e "$CR_IP\n$CR_PSSWD\ny" | ./crclient -u $CR_ID -i lo > /dev/null #using /dev/null suppresses output from stdout, except from stderr
+	RET=$?
+	unset CR_IP
+	unset CR_PSSWD
+	unset CR_ID
+	return $RET;
+fi
+
+
+./crclient -u "'cat CR_ID.txt'" -i lo > /dev/null
+return $?
--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 if [ -d ~/bin ] ; then
 	if [ -e ~/bin/crconn ] ; then
-		echo -e "Already installed!!"
+		echo "Already installed!!"
 	else
 		cd ~/bin
 		ln -s "$OLDPWD/exec.sh" crconn
 		cd "$OLDPWD"
 		chmod +x exec.sh
-		echo -e "Install Success. Restart the shell to use 'crconn' to sign into CyberRoam."
+		echo -e "Install Success !\nRestart the shell to use 'crconn' to sign into CyberRoam."
 	fi
 else
 	mkdir ~/bin
@@ -14,6 +14,6 @@ else
 	ln -s "$OLDPWD/exec.sh" crconn
 	cd "$OLDPWD"
 	chmod +x exec.sh
-	echo -e "Install Success. Restart the shell to use 'crconn' to sign into CyberRoam."
+	echo -e "Install Success !\nRestart the shell to use 'crconn' to sign into CyberRoam."
 fi
 
