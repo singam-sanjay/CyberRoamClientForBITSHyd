dada13bfcf4bdcd818df747b71deae41fc317d38
Sanjay <sssv96@gmail.com>
2015-06-12 17:51:36 +0530

    Possible fix for issue 1, replaced "'" with "`"

--- exec.sh
+++ exec.sh
@@ -12,7 +12,7 @@ fi
 if [ ! -e ~/CyberClient.conf ] ; then
 	CR_IP="172.16.0.30" #default is of BITS Hyderabad
 	echo -n "Password :"; read CR_PSSWD;
-	CR_ID='cat CR_ID.txt'
+	CR_ID=`cat CR_ID.txt`
 	echo -e "$CR_IP\n$CR_PSSWD\ny" | ./crclient -u $CR_ID -i lo > /dev/null #using /dev/null suppresses output from stdout, except from stderr
 	RET=$?
 	unset CR_IP
